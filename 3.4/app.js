f3c4c25fa361be13980734bcfc800af3b2e1ca46
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-01-01 23:56:57 +0900

    Feat:arranged files

---
+++ 3.4/app.js
@@ -0,0 +1,11 @@
+import { createStore } from "./redux.js";
+import { reducer } from "./reducer.js";
+
+const store = createStore(reducer);
+
+store.subscribe(function () {
+  console.log(store.getState());
+});
+
+store.send({ type: "increase" });
+store.send({ type: "increase" });
